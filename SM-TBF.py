7e36b3de13e2a7a7b800de1fa612d159cb7ee132
alelucadtb <157207954+alelucadtb@users.noreply.github.com>
2024-12-12 12:13:41 +0100

    someChanges
    Correct some problems with TBF

--- SM-TBF.py
+++ SM-TBF.py
@@ -6,7 +6,6 @@ from pathlib import Path
 def sourceModule(Ton, Toff, tau, B, L, Nc, namefile):
     # number of batches in a ON period
     z = math.floor(Ton / tau)
-    # number of cycles that starts from zero and then should be increase
     ti = []
     for value in range(0, Nc + z*10000):
         for time in range(0, z):
@@ -39,6 +38,11 @@ def sourceModule(Ton, Toff, tau, B, L, Nc, namefile):
         for line in mat:
             np.savetxt(f, line, fmt='%.2f')
 
+'''
+for labeling the packets:
+- 1 if they are compiant
+- 0 if they are excess
+'''
 def labeling(capacity, element, vector):
     if capacity - element >= 0:
         capacity = capacity - element
@@ -47,49 +51,54 @@ def labeling(capacity, element, vector):
         vector.append(0)
     return capacity
 
+# for manage the variable time and the token bucket generation rate (rho)
 def checkingTime(time, element, capacity, rho, b):
     if time + element < 1:
         time = time + element
     else:
-        time = time + element - int(element)
-        add = rho * int(element)
+        # how many tokens must be sum to the token bucket
+        time = time + element
+        add = rho * int(time)
         if capacity + add < b:
             capacity = capacity + add
         else:
             capacity = b
+        time = time % 1   
     return time, capacity
 
-
+def compliantPackets(vector):
+    sum = 0
+    for element in vector:
+        if element == 1:
+            sum = sum + 1
+    return sum/len(vector)
+    
 def tokenBucketFilter(namefile, rho, b, b0):
     mat = np.loadtxt(namefile , delimiter=' ')
     capacity = b0
     # it is a sufficient high value
-    elm0prev = 10000000
-    time = 0
+    time = 0.0
     # column vector in which it is stored the result
     F = []
 
     for elm in mat:
-        # elm[0] is the arrival time
-        # elm[1] is the batch size --> number of packets for an arrival
-        # elm[2] is the aggregated workload 
-        if elm[0] <= elm0prev:
+        '''
+        different elements of elm mean:
+        - elm[0] is the arrival time
+        - elm[1] is the batch size --> number of packets for an arrival
+        - elm[2] is the aggregated workload 
+        '''
+        # the first things to do is to checking the time
+        time, capacity = checkingTime(time, elm[0], capacity, rho, b)
+        for num in range(0, int(elm[1])):
+            # check for every packet
+            capacity = labeling(capacity, int(elm[2])/int(elm[1]), F)
             # print(capacity)
-            for num in range(0, int(elm[1])):
-                # check for every packet
-                capacity = labeling(capacity, elm[2]/elm[1], F)
-            time, capacity = checkingTime(time, elm[0], capacity, rho, b)
-            # print(capacity)
-        else:
-            # print(capacity)
-            time, capacity = checkingTime(time, elm[0], capacity, rho, b)
-            # print(capacity)
-        elm0prev = elm[0]
 
-    for element in F:
-        print(element)
+    for line in F:
+        print(line)
 
 
 
 sourceModule(1, 1, 0.2, 3, 1000, 10000, 'output.txt')
-tokenBucketFilter('output.txt', 8000, 3000, 3000)
+tokenBucketFilter('SourceTrafficTrace.txt', 8000, 3000, 3000)
